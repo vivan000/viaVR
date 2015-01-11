ef515189ce73fbf8fdeddd8e92eaa749a7f1c26d
vivan000 <ivan.valiulin@gmail.com>
2015-01-12 01:28:28 +0300

    fp textures don't work for some reason

--- jni/frames/frameGPUi.cpp
+++ jni/frames/frameGPUi.cpp
@@ -5,8 +5,8 @@ frameGPUi::frameGPUi (int w, int h, bool half) {
 	glBindTexture (GL_TEXTURE_2D, plane);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexImage2D (GL_TEXTURE_2D, 0, half ? GL_HALF_FLOAT_OES : GL_FLOAT, w, h, 0,
-		GL_RGBA, half ? GL_HALF_FLOAT_OES : GL_FLOAT, NULL);
+	//glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, half ? GL_HALF_FLOAT_OES : GL_FLOAT, NULL);
+	glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 
 	timecode = 0;
 }

cee2ee68f6b6b9fcf511a5d3cdce7d21312f7eb3
vivan000 <ivan.valiulin@gmail.com>
2015-01-18 05:15:44 +0300

    Add licenses

--- jni/shaders/displayFrag.h
+++ jni/shaders/displayFrag.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2015 Ivan Valiulin
+ *
+ * This file is part of viaVR.
+ *
+ * viaVR is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * viaVR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with viaVR. If not, see http://www.gnu.org/licenses
+ */
+
 "#version 300 es														\n"
 "precision lowp float;													\n"
 "																		\n"

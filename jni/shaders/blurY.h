162d51d61c6dc6a9d9bd74e512cbbd2cdc99d52a
vivan000 <ivan.valiulin@gmail.com>
2015-01-26 22:55:49 +0300

    Blur shaders (for debanding)

---
+++ jni/shaders/blurY.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015 Ivan Valiulin
+ *
+ * This file is part of viaVR.
+ *
+ * viaVR is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * viaVR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with viaVR. If not, see http://www.gnu.org/licenses
+ */
+
+R"(#version 300 es
+precision %s float;
+#define taps %i
+
+uniform sampler2D video;
+uniform vec3 pitch;
+uniform float weight[taps];
+uniform float offset[taps];
+in vec2 coord;
+out vec4 outColor;
+
+void main () {
+	vec3 sum = vec3 (0.0);
+
+	sum += texture (video, vec2 (coord.x, coord.y - offset[0])).rgb * weight[0];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[0])).rgb * weight[0];
+
+#if taps>1
+	sum += texture (video, vec2 (coord.x, coord.y - offset[1])).rgb * weight[1];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[1])).rgb * weight[1];
+#endif
+#if taps>2
+	sum += texture (video, vec2 (coord.x, coord.y - offset[2])).rgb * weight[2];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[2])).rgb * weight[2];
+#endif
+#if taps>3
+	sum += texture (video, vec2 (coord.x, coord.y - offset[3])).rgb * weight[3];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[3])).rgb * weight[3];
+#endif
+#if taps>4
+	sum += texture (video, vec2 (coord.x, coord.y - offset[4])).rgb * weight[4];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[4])).rgb * weight[4];
+#endif
+#if taps>5
+	sum += texture (video, vec2 (coord.x, coord.y - offset[5])).rgb * weight[5];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[5])).rgb * weight[5];
+#endif
+#if taps>6
+	sum += texture (video, vec2 (coord.x, coord.y - offset[6])).rgb * weight[6];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[6])).rgb * weight[6];
+#endif
+#if taps>7
+	sum += texture (video, vec2 (coord.x, coord.y - offset[7])).rgb * weight[7];
+	sum += texture (video, vec2 (coord.x, coord.y + offset[7])).rgb * weight[7];
+#endif
+
+	outColor = vec4 (sum, 1.0);
+})";

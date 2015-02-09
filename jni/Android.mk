489875866ab76924bdeb39385cc73895ea1055d1
vivan000 <ivan.valiulin@gmail.com>
2015-02-09 15:51:37 +0300

    Add pattern generator to the makefile

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := viaVR
 LOCAL_SRC_FILES := rendererWrapper.cpp videoRenderer.cpp videoInfo.cpp log.cpp
-LOCAL_SRC_FILES += decoders/rawDecoder.cpp
+LOCAL_SRC_FILES += decoders/rawDecoder.cpp decoders/patternGenerator.cpp
 LOCAL_SRC_FILES += threads/helpers/renderingPass.cpp threads/helpers/shader.cpp
 LOCAL_SRC_FILES += threads/decoder.cpp threads/uploader.cpp threads/renderer.cpp threads/presenter.cpp
 LOCAL_SRC_FILES += frames/frameCPU.cpp frames/frameGPUu.cpp frames/frameGPUi.cpp frames/frameGPUo.cpp 

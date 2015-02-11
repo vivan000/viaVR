02c2e79694dc7c6aae256a22cec3d8943819ada3
vivan000 <ivan.valiulin@gmail.com>
2015-02-11 07:24:32 +0300

    Remove unneeded semicolon

--- jni/rendererWrapper.h
+++ jni/rendererWrapper.h
@@ -4,4 +4,4 @@ extern "C" {
 	JNIEXPORT void JNICALL Java_vivan_viavr_MainActivity_nativeOnPause (JNIEnv* jenv, jobject obj);
 	JNIEXPORT void JNICALL Java_vivan_viavr_MainActivity_nativeOnStop (JNIEnv* jenv, jobject obj);
 	JNIEXPORT void JNICALL Java_vivan_viavr_MainActivity_nativeSetSurface (JNIEnv* jenv, jobject obj, jobject surface);
-};
+}

79b5e5a2625759aabf18c3553d986e9b330ed1a9
vivan000 <ivan.valiulin@gmail.com>
2015-02-18 10:06:17 +0300

    Remove dead code

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE    := viaVR
 LOCAL_SRC_FILES := rendererWrapper.cpp videoRenderer.cpp videoInfo.cpp log.cpp
 LOCAL_SRC_FILES += decoders/rawDecoder.cpp decoders/patternGenerator.cpp
 LOCAL_SRC_FILES += threads/helpers/renderingPass.cpp threads/helpers/shaderLoader.cpp
-LOCAL_SRC_FILES += threads/decoder.cpp threads/uploader.cpp threads/renderer.cpp threads/presenter.cpp
+LOCAL_SRC_FILES += threads/decoder.cpp threads/renderer.cpp threads/presenter.cpp
 LOCAL_SRC_FILES += frames/frameCPU.cpp frames/frameGPUu.cpp frames/frameGPUi.cpp frames/frameGPUo.cpp 
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv3
 LOCAL_CPPFLAGS := -std=c++11 
